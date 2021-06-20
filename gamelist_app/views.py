855a94cd9115373378f94e222a2e338fcf3264c5
Daniel Thompson <dthompson0190@gmail.com>
2021-06-20 13:25:57 -0500

    added site logo and updated settings

--- gamelist_app/views.py
+++ gamelist_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request,"base.html")
